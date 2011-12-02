e3861fdcaad2db720d9d8858a66e267224f3da55
dtscraft <dtscraft@gmail.com>
2011-12-02 01:56:27 -0800

    more templates and added concentration

--- wdd-app/server.py
+++ wdd-app/server.py
@@ -16,26 +16,48 @@ return user
 else:
 return None
 """
+###class BaseHandler
+imgsrc = ""    
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class LoginHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("login.html")
 
+class FooterHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("footer.html")
+class GameHandler(tornado.web.RequestHandler):
+      def get_template_path(self):
+        """Override to customize template path for each handler.
+        By default, we use the 'template_path' application setting.
+        Return None to load templates relative to the calling file.
+        """
+        return self.application.settings.get("game_path")
+      def get(self):
+        self.render("concentration.html")
+  
 application = tornado.web.Application([
 
-  # WDD TODO
-  # Add more route(s) to handle the doubling
-
-  (r"/",       MainHandler),
-
-  ###### routes end here ######
+        # WDD TODO
+        # Add more route(s) to handle the doubling
+        
+ (r"/",MainHandler),
+ (r"/login",LoginHandler),
+ (r"/footer", FooterHandler),
+ (r"/games",GameHandler)
+   ###### routes end here ######
 
 #########################
 # Don't change this     #
 #########################
 ], debug=True,
-   template_path=os.path.join(os.path.dirname(__file__), "templates"),
-   static_path=os.path.join(os.path.dirname(__file__), "static"),
+  template_path=os.path.join(os.path.dirname(__file__), "templates"),
+  static_path=os.path.join(os.path.dirname(__file__), "static"),
+  game_path=os.path.join(os.path.dirname(__file__), "game"),                              
+
 )
 
 if __name__ == "__main__":
